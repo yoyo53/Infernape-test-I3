b32251059edcaec532c646178ca0c24382d174ab
Dimitri Merejkowsky <dimitri@dmerej.info>
2025-01-26 15:05:16 +0100

    Add java version

---
+++ java/src/test/java/info/dmerej/AddTeamTest.java
@@ -0,0 +1,47 @@
+package info.dmerej;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AddTeamTest {
+    @Test
+    void test_add_team() {
+        // Use playwright driver to get a browser and open a new page
+        var playwright = Playwright.create();
+        var launchOptions = new BrowserType.LaunchOptions().setHeadless(false)
+            .setSlowMo(1000); // Remove this when you're done debugging
+        var browser = playwright.chromium().launch(launchOptions);
+
+        // Set base URL for the new context
+        var contextOptions = new Browser.NewContextOptions();
+        contextOptions.setBaseURL("https://<letter>.lsi1.hr.dmerej.info");
+        var context = browser.newContext(contextOptions);
+
+        var page = context.newPage();
+
+        // Reset database
+        page.navigate("/reset_db");
+        var proceedButton = page.locator("button:has-text('proceed')");
+        proceedButton.click();
+        page.navigate("/");
+
+        // Add a new team
+        page.navigate("/add_team");
+        var nameInput = page.locator("input[name=\"name\"]");
+        var teamName = "my team";
+        nameInput.fill(teamName);
+        page.click("text='Add'");
+
+        // Check that the team has been added
+        page.navigate("/teams");
+
+        // Check the new team is there
+        String selector = String.format("td:has-text('%s')", teamName);
+        var isVisible = page.isVisible(selector);
+        assertTrue(isVisible);
+    }
+}
